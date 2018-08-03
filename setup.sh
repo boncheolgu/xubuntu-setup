86891e46cdb1f21c7acf29cb6a0c7f8cf079e6f4
Boncheol Gu <boncheol.gu@gmail.com>
2018-08-04 02:24:26 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-sudo apt-get install emacs25 xcape gmrun stalonetray unclutter xmonad xmobar scrot shutter fonts-nanum-coding
+sudo apt-get install emacs25 xcape gmrun stalonetray unclutter xmonad xmobar scrot shutter fonts-nanum-coding ninja-build
 ln -s ${PWD}/.emacs.d  ${HOME}
 ln -s ${PWD}/.xmonad ${HOME}
