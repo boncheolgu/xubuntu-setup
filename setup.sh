216caf226d67e34e151d9e026f5e8d6d0891264b
Boncheol Gu <boncheol.gu@gmail.com>
2018-08-04 01:57:37 +0900

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+sudo apt-get install emacs25 xcape gmrun stalonetray unclutter xmonad xmobar scrot shutter fonts-nanum-coding
+ln -s ${PWD}/.emacs.d  ${HOME}
+ln -s ${PWD}/.xmonad ${HOME}
