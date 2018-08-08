69b38f9e9eb14ecb1c8e5acf0bce6b8f61581275
Boncheol Gu <boncheol.gu@gmail.com>
2018-08-08 20:07:21 +0900

    Add some rc files

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@
 sudo apt-get install emacs25 xcape gmrun stalonetray unclutter xmonad xmobar scrot shutter fonts-nanum-coding ninja-build
 ln -s ${PWD}/.emacs.d  ${HOME}
 ln -s ${PWD}/.xmonad ${HOME}
+ln -s ${PWD}/.gmrunrc ${HOME}
+ln -s ${PWD}/.stalonetrayrc ${HOME}
